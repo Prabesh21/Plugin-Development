47b1a82a975e545b42d9037dc270038fce521512
Prabesh21 <prabeshupreti21@gmail.com>
2020-08-12 09:51:28 +0545

    email sending from wordpress

---
+++ email-sender/email-sender.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Plugin Name:       Email Sender
+ * Plugin URI:        https://example.com/plugins/the-basics/
+ * Description:       Handle the basics with this plugin.
+ * Version:           1.10.3
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            John Smith
+ * Author URI:        https://author.example.com/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       my-basics-plugin
+ * Domain Path:       /languages
+ */
+function menu_adder(){
+    add_menu_page('Menu Title', 'Mailsender', 'manage_options', 'send-mail', 'fn_cb');
+    add_submenu_page( 'send-mail', 'Send Mail', 'Settings menu label', 'manage_options', 'email-slug', 'mail_cb');
+  }
+  add_action('admin_menu', 'menu_adder');
+  add_action( 'admin_init', 'save_setting_details' );
+  function save_setting_details(){
+    if(isset($_POST['my_submit_button'])){
+        $my_email= $_POST['my_email'];
+        $my_subject = $_POST['my_subject'];
+        $my_message= $_POST['my_message'];
+        error_log($my_email);
+        error_log($my_message);
+        error_log($my_subject);        
+        wp_mail( $my_email, $my_subject, $my_message );
+    }
+}
+   
+  function fn_cb(){
+          
+  }
+  function mail_cb(){
+    ?>
+    <div class="wrap">
+        <h1>My Settings</h1>
+        <form method="post">
+        Send Email To: <input type="email" id="my_email" name="my_email" /><br>
+        Message: <input type="text" id="my_message" name="my_message"  /><br>
+        Subject: <input type="text" id="my_subject" name="my_subject"  /><br>
+        <input type="submit" name="my_submit_button" value="Save"/>
+        </form>
+    </div>
+    <?php
+  }
